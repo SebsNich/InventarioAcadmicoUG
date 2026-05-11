16df33e9a5ee0fa831f17dbd302b04ed7dd7ddd5
TuNombre <tuemail@ejemplo.com>
2026-05-11 09:50:22 -0500

    Cambio Interfaz

--- app/src/main/java/com/dam/inventarioacadmicoug/FormItemActivity.java
+++ app/src/main/java/com/dam/inventarioacadmicoug/FormItemActivity.java
@@ -1,4 +1,12 @@
 package com.dam.inventarioacadmicoug;
 
-public class FormItemActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormItemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_item);
+    }
 }
--- app/src/main/java/com/dam/inventarioacadmicoug/ListaItemsActivity.java
+++ app/src/main/java/com/dam/inventarioacadmicoug/ListaItemsActivity.java
@@ -1,4 +1,12 @@
 package com.dam.inventarioacadmicoug;
 
-public class ListaItemsActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListaItemsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_items);
+    }
 }
